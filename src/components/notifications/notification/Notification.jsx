58dc48dffc31891b4e2acf174b41816ce94bdf41
Maynor Lopez <maynor94l39@gmail.com>
2026-09-24 23:34:53 -0600

    Organize info in a json and then render info using map and notification component

--- src/components/notifications/notification/Notification.jsx
+++ src/components/notifications/notification/Notification.jsx
@@ -1,7 +1,8 @@
 import NotificationAvatar from "./NotificationAvatar";
 import NotificationBody from "./NotificationBody";
+import NotificationMessage from "./NotificationMessage";
 
-function Notification({avatar, isNew, type, name, post, group, time, picture, children }){
+function Notification({avatar, isNew, type, name, post, group, time, picture, message }){
     return(
             <article className={`w-full flex flex-col gap-2 justify-between align-center rounded-lg p-4 
             ${isNew ? "bg-Navy-100" : "bg-White"}`}>
@@ -9,7 +10,7 @@ function Notification({avatar, isNew, type, name, post, group, time, picture, ch
                     <NotificationAvatar avatar={avatar}/>
                     <NotificationBody group={group} isNew={isNew} type={type} name={name} post={post} time={time} picture={picture}/>
                 </div>
-                {children}
+                {message ? <NotificationMessage message={message}/> : null}
             </article>
         
     )
